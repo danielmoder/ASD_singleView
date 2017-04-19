3ebc44836556d2977e8e7c5e741522681bf9a950
Daniel Moder <daniel.j.moder.18@dartmouth.edu>
2017-04-18 20:20:33 -0400

    Initial Commit

---
+++ ASD_SingleViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ASD_SingleViewDemo
+//
+//  Created by Daniel Moder on 4/18/17.
+//  Copyright © 2017 Dali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
